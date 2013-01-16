2cdcc25f44f1d5fe6edc6137720493bf609ea484
Aman Gupta <aman@tmm1.net>
2013-01-15 20:07:01 -0800

    slightly better test with method calls

--- test.rb
+++ test.rb
@@ -1,27 +1,33 @@
 $:.unshift 'ext'
 require 'rblineprof'
 
-profile = lineprof(/./) do
+def inner
+  sleep 0.001
+  1*2*3
+  4*5*6
+  7*8*9*10*11*12*13*14*15
+  2**32
+  2**128
+end
+
+def outer
   sleep 0.001
 
   100.times do
-
-    sleep 0.001
-    1*2*3
-    4*5*6
-    7*8*9*10*11*12*13*14*15
-    2**32
-    2**128
-
+    inner
   end
+
+  inner
+end
+
+profile = lineprof(/./) do
+  outer
 end
 
 File.readlines(__FILE__).each_with_index do |line, num|
   if (sample = profile[__FILE__][num+1]) > 0
-    # printf "% 7d |  %s", sample, line
     printf "% 8.1fms |  %s", sample/1000.0, line
   else
-    # printf "        | %s", line
     printf "           | %s", line
   end
 end
