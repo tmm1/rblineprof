53f78556d59c52bc3d9e73da1201c26dbfeadd89
Aman Gupta <aman@tmm1.net>
2013-02-11 01:37:19 -0800

    fallback to filename when filepath is nil

--- ext/rblineprof.c
+++ ext/rblineprof.c
@@ -261,9 +261,11 @@ profiler_hook(rb_event_flag_t event, NODE *node, VALUE self, ID mid, VALUE klass
   rb_thread_t *th = GET_THREAD();
   rb_control_frame_t *cfp = rb_vm_get_caller(th, th->cfp, mid);
   if (!cfp) return;
-  if (!RTEST(cfp->iseq->filepath)) return;
 
-  file = StringValueCStr(cfp->iseq->filepath);
+  if (RTEST(cfp->iseq->filepath))
+    file = StringValueCStr(cfp->iseq->filepath);
+  else
+    file = StringValueCStr(cfp->iseq->filename);
   line = rb_vm_get_sourceline(cfp);
 #endif
 
