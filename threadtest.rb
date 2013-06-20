8f5be559d5882bf25daf7fe706d940ac4113b117
Matt Burke <spraints@gmail.com>
2013-06-20 11:02:03 -0400

    Make it easier to compare manually instrumented lines vs rblineprof.

--- threadtest.rb
+++ threadtest.rb
@@ -1,25 +1,40 @@
 require 'rblineprof'
 
+$line_times = Hash.new(0.0)
+
 def main
-  lines = File.readlines(__FILE__)
-  profile = log('total') { getprofile }
-  line_profs = profile[File.expand_path(__FILE__)].drop(1).take(lines.length)
-  line_profs.zip(lines).each do |(wall, cpu, calls, allocations), content|
-    printf "%8.1fms / %8.1fms (% 5d) (% 6d) | %s", wall/1000.0, cpu/1000.0, calls, allocations, content
+  profile = log(__LINE__) { getprofile }
+  profile = profile.values.first
+  File.readlines(__FILE__).each_with_index do |content, i|
+    lineno = i + 1
+    line_time =
+      if $line_times.has_key?(lineno)
+        sprintf('%6.3fms', $line_times[lineno] * 1000.0)
+      else
+        (' '*8)
+      end
+    prof_time =
+      if profile[lineno] && profile[lineno].any? { |x| x > 0 }
+        wall, cpu, _ = profile[lineno]
+        sprintf('%6.3fms (%6.3fms cpu)', wall / 1000.0, cpu / 1000.0)
+      else
+        ' '*23
+      end
+    puts "#{line_time} vs #{prof_time} | #{'%2d' % lineno} | #{content}"
   end
 end
 
 def getprofile
-  lineprof(/./) do
+  lineprof(File.expand_path(__FILE__)) do
     t = Thread.new do
-      log('thread') do
+      log(__LINE__) do
         thread_work
       end
     end
-    log('main') do
+    log(__LINE__) do
       main_work
     end
-    log('waitthread') do
+    log(__LINE__) do
       t.join
     end
   end
@@ -34,16 +49,15 @@ def main_work
 end
 
 def work(n)
-  log('sleep') { sleep 0.01          }
-  log('math')  { n.times { 2**1024 } }
+  log(__LINE__) { sleep 0.01          }
+  log(__LINE__) { n.times { 2**1024 } }
 end
 
-def log(label)
+def log(line)
   start = Time.now
   yield
 ensure
-  elapsed = Time.now - start
-  printf "%s: %.1fms\n", label, elapsed*1000.0
+  $line_times[line] += Time.now - start
 end
 
 main
