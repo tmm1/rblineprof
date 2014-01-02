729570000c5adf01ef36350488616183098a4821
Aman Gupta <aman@tmm1.net>
2014-01-01 23:45:24 -0800

    barebones test suite

---
+++ test/test_lineprof.rb
@@ -0,0 +1,33 @@
+$:.unshift File.expand_path('../../lib', __FILE__)
+require 'rblineprof'
+require 'test/unit'
+
+class LineProfTest < Test::Unit::TestCase
+  def test_real
+    profile = lineprof(/./) do
+      sleep 0.001
+    end
+
+    line = profile[__FILE__][__LINE__-3]
+    assert_in_delta 1000, line[0], 300
+    assert_equal 1, line[2]
+  end
+
+  def test_cpu
+    profile = lineprof(/./) do
+      (fibonacci = Hash.new{ |h,k| h[k] = k < 2 ?  k : h[k-1] + h[k-2] })[500]
+    end
+
+    line = profile[__FILE__][__LINE__-3]
+    assert_operator line[1], :>, 1000
+  end
+
+  def test_objects
+    profile = lineprof(/./) do
+      100.times{ "str" }
+    end
+
+    line = profile[__FILE__][__LINE__-3]
+    assert_equal 100, line[3]
+  end
+end
